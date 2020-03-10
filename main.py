7bc2c8b8abfb9585315a106d4b3880ca92086e9e
Austin Shymanski <austin.shymanski@gmail.com>
2020-03-10 17:36:06 -0400

    Starting to work with data types and using math

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 names = input('What is your name?')
 
 print("Hey " + name)
+
+#Fundamental Data Types
+
+print(2 + 4)
+print(type(2 + 4))
+print(type(2 / 4)) #0.5 = float
+
+#Adding 2 floating numbers together that equal an interger still
+#saves as a float ex 9.9 + 1.1
+
+print(2 ** 3) # = 8 (2x2x2)
+print(5 // 4) # = 1 (how many times can 4 go into 5)
+print(6 % 4) # = 2 (What is left after I take 4 out of 6)
+
+
