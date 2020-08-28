3d3b00d8620882c7a5c5ee8c60eff8e71d6c3cf7
Austin <austin.shymanski@gmail.com>
2020-08-28 13:08:12 -0400

    list slicing

--- index.py
+++ index.py
@@ -1,4 +1,16 @@
-li = [1,2,3,4,5]
-li2 = ['a','b','c','d']
-li3 = [1,2,'a', True]
-print(li[2])
+# li = [1,2,3,4,5]
+# li2 = ['a','b','c','d']
+# li3 = [1,2,'a', True]
+# print(li[2])
+
+# List slicing
+amazon_cart = [
+	'notebooks',
+	'sunglasses',
+	'toys',
+	'grapes'
+]
+amazon_cart[0] = 'laptop'
+print(amazon_cart[0::2])
+new_cart = amazon_cart[:]
+# amazon_cart stays the same and new_cart makes a copy of it using [:]
