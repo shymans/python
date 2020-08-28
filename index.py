c92bb2c63a058e247ab774884d8e8c6a2586d0e3
Austin <austin.shymanski@gmail.com>
2020-08-28 13:30:50 -0400

    list methods

--- index.py
+++ index.py
@@ -14,3 +14,13 @@ amazon_cart[0] = 'laptop'
 print(amazon_cart[0::2])
 new_cart = amazon_cart[:]
 # amazon_cart stays the same and new_cart makes a copy of it using [:]
+
+basket = [1,2,3,4,5]
+basket.append(100)
+basket.insert(4, 200)
+basket.extend([300])
+basket.pop() #removes the last item in the list
+basket.pop(0) #removes the specific index
+basket.remove(3) #removes specific value
+basket.clear() #removes everything in the list
+print(basket)
